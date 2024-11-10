423cf1ed3f154421011bcb3e30841cea5b96802b
Grégory EPLE <gregory.eple@epitech.eu>
2024-11-10 19:56:48 +0100

    Update BotConfig.py

--- BotConfig.py
+++ BotConfig.py
@@ -1,6 +1,6 @@
 TOKEN = "DISCORD_BOT_TOKEN" # Token of the Discord bot (Remember to never share this token)
 SERVER_IP = "MINECRAFT_SERVER_IP" # IP of the Minecraft server
 BOT_NAME = "" # Name of the bot that will be displayed on Discord (Leave empty to not change the bot's name)
-UPDATE_TIME = 300 # How often the bot's status should be updated (in seconds)
+UPDATE_TIMER = 300 # How often the bot's status should be updated (in seconds)
 SHOW_UPDATE_TIMER = False # Should the update timer be added to the bot's status?
 SHOW_LAST_UPDATE_TIME = False # Should the time of the last update be added to the bot's status?
